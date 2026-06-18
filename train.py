aa5e1f55f3052ef4a7a323e617010557665e50d6
algorythm3000 <chin.sei@laposte.net>
2026-06-18 23:48:06 +0200

    stupid commit

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-# calls graph_classification.py or node_classification.py to train the model
+# calls graph_classification.py or node_classification.py to train the model test 2
+
 import argparse
 import os
 import sys
